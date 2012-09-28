81c1923d3329653af77ea0d6b3c86fe4c2669def
Taylor Allred <taylorallred@moneydesktop.com>
2012-09-28 09:35:24 -0600

    fixed dependent bug

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,7 +1,7 @@
 class Vehicle < ActiveRecord::Base
   attr_accessible :make, :model, :oil_type, :year, :license_plate, :notes, :vin_number, :owner_id, :color
   
-  has_many :appointments
+  has_many :appointments, :dependent => :destroy
   belongs_to :owner
   
 
