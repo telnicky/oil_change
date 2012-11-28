98f71f40b6c84e2a32cc0c57fd6ccaafd3321e9b
Taylor Allred <taylorallred@moneydesktop.com>
2012-11-28 16:55:31 -0700

    not much new

--- app/assets/javascripts/main/owner.js
+++ app/assets/javascripts/main/owner.js
@@ -1,24 +1,19 @@
 $(function () {
-  require([ //owner
-            'views/OwnersInfoView', 
-            'models/OwnerModel',
-            //appointments
-            'views/OwnerAppointmentsView', 
-            'views/OwnerAppointmentItemView', 
-            'collections/Appointments'
+  require([ 'views/OwnersInfoView', 'models/OwnerModel',
+            'views/OwnerAppointmentsView', 'views/OwnerAppointmentItemView', 'collections/Appointments'
             //cc & payment info
             //vehicles
           ],
     function (OwnersInfoView, OwnerModel, OwnerAppointmentsView, OwnerAppItemView, AppointmentsCollection) {
 ///////////// Display & edit owner info///////////
-      var ownersInfo = new OwnerModel(window.jsonData)
+      var ownersInfo = new OwnerModel(window.jsonData);
       
       var ownerView = new OwnersInfoView({
         model:  ownersInfo
       });
       $('.owners-info-area').append(ownerView.el);
 ///////////// Display Appointments
-      var appointments = new AppointmentsCollection(window.jsonData.available_appointments)
+      var appointments = new AppointmentsCollection(window.jsonData.available_appointments);
       var ownerAppView = new OwnerAppointmentsView({
         collection: appointments,
         itemView: OwnerAppItemView
@@ -27,5 +22,5 @@ $(function () {
       $('.owners-appointment-list').append(ownerAppView.el);
 ///////////// Display Vehicles
       
-    })
+    });
 });
